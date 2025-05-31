6f8c3e309620d6cde0852b45cfc48ae5a2959415
Daniel <danielmartiger@gmail.com>
2025-05-30 23:42:44 -0400

    actividad 5
    5.Adjuntar comentario de una y de varias líneas un su código.  Imprimir la fecha y hora del sistema.

--- Tarea1/MyHomework/MyFirstHomework/Program.cs
+++ Tarea1/MyHomework/MyFirstHomework/Program.cs
@@ -1,10 +1,15 @@
 ﻿//Daniel Martinez 20241745
 Console.WriteLine("Tarea 1:");
-Console.WriteLine("Actividad 4:");
-float sumando= 10152466.25f;
-byte total = (byte)(sumando + 5);
-
-
-Console.WriteLine(total);
+Console.WriteLine("Actividad 5:");
+/*
+int entero = 1;
+Console.WriteLine(entero);
+entero++;
+Console.WriteLine(entero);
+entero--;
+Console.WriteLine(entero);
+entero += 6;
+Console.WriteLine(entero);
+*/
 Console.ReadKey();
 
