62329138dd769ee3f3bd8355fdc772225829107c
Daniel <danielmartiger@gmail.com>
2025-05-30 23:09:16 -0400

    actividad 2
    2.Buscar cómo se declara una constante en C#

--- Tarea1/MyHomework/MyFirstHomework/Program.cs
+++ Tarea1/MyHomework/MyFirstHomework/Program.cs
@@ -1,17 +1,7 @@
 ﻿//Daniel Martinez 20241745
 Console.WriteLine("Tarea 1:");
-int number;
-number = 1;
-float value;
-value = 10152466.25f;
-char letter = 'D';
-letter = 'D';
-string name;
-name = "Daniel";
-Console.WriteLine(number);
-Console.WriteLine(value);
-Console.WriteLine(letter);
-Console.WriteLine($"Mi nombre es {name}");
-
+Console.WriteLine("Actividad 2:");
+const double gravedad=9.8;
+Console.WriteLine(gravedad);
 Console.ReadKey();
 
