217af2a3de71714dbca3b8ed69dcc83995e72608
Daniel <danielmartiger@gmail.com>
2025-05-30 22:51:31 -0400

    actividad 1
    1.Declarar variable de los diferentes tipos, asignarles valor e imprimir el valor.

---
+++ Tarea1/MyHomework/MyFirstHomework/Program.cs
@@ -0,0 +1,17 @@
+﻿//Daniel Martinez 20241745
+Console.WriteLine("Tarea 1:");
+int number;
+number = 1;
+float value;
+value = 10152466.25f;
+char letter = 'D';
+letter = 'D';
+string name;
+name = "Daniel";
+Console.WriteLine(number);
+Console.WriteLine(value);
+Console.WriteLine(letter);
+Console.WriteLine($"Mi nombre es {name}");
+
+Console.ReadKey();
+
