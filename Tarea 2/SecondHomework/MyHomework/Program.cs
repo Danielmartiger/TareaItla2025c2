316a9293d2611525f80f9b06e9b1520ee235f337
Daniel <danielmartiger@gmail.com>
2025-06-12 01:29:02 -0400

    programa sencillo

---
+++ Tarea 2/SecondHomework/MyHomework/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int num;
+Console.WriteLine("Escribe un numero");
+int.TryParse(Console.ReadLine(), out num);
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Es par");
+}
+else
+{
+    Console.WriteLine("Es impar");
+}
